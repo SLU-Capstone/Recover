8e48a989c28271a8afec8e9dfc8568a8a51b5442
Luke Reichold <lukereichold@gmail.com>
2016-03-31 22:58:47 -0500

    Add 500 error page and handler; add glyph to "add patient" page header

--- recover/views/management.py
+++ recover/views/management.py
@@ -135,6 +135,12 @@ def unauthorized():
 def not_found(error):
     return render_template('404.html'), 404
 
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
 # Admin section - WIP #
 from recover.forms import AdminViewer
 
