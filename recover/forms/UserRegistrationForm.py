66d5b8b0fc5b86dcebcc872d6cf2a92a5dbe566a
Luke Reichold <lukereichold@gmail.com>
2016-04-06 17:33:31 -0500

    Add placeholder values to WTF form fields

--- recover/forms/UserRegistrationForm.py
+++ recover/forms/UserRegistrationForm.py
@@ -9,7 +9,7 @@ class UserRegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     full_name = StringField('Full Name', [validators.Length(min=2, max=70)])
     email = StringField('Email Address', [validators.Length(min=5, max=35)])
-    password = PasswordField('New Password', [
+    password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
