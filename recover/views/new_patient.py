2336cb9bd55c6ea145008ef1e5b90c5853b50cbe
Matthew Meyer <mattchainsaw@gmail.com>
2016-04-11 16:13:44 -0500

    pull activity data on creation

--- recover/views/new_patient.py
+++ recover/views/new_patient.py
@@ -109,7 +109,9 @@ def authorize_new_patient():
 
             # By default, get 5 days worth of data for the brand new patient
             new_patient_data = PatientData(new_patient)
-            if not new_patient_data.get_heart_rate_data_for_x_days(5):
+            retrieved = new_patient_data.get_heart_rate_data_for_x_days(5)
+            retrieved = retrieved and new_patient_data.get_activity_data_for_x_days(5)
+            if not retrieved:
                 flash('Could not retrieve Patient data', 'warning')
                 return redirect('/')
 
