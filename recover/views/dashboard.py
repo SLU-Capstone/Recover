a6ccb1d1eb05fc6830149ce2706c95b91eb2cabb
Matthew Meyer <mattchainsaw@gmail.com>
2016-04-19 22:03:34 -0500

    allow celery to know the object

--- recover/views/dashboard.py
+++ recover/views/dashboard.py
@@ -147,8 +147,8 @@ def patient_profile(slug):
             last = datetime.datetime.strptime(last_pull, '%Y-%m-%d')
             days = (today - last).days
             patient_data = PatientData(patient)
-            patient_data.get_heart_rate_data_for_x_days.delay(days)
-            patient_data.get_activity_data_for_x_days.delay(days)
+            PatientData.get_heart_rate_data_for_x_days.delay(patient_data, days)
+            PatientData.get_activity_data_for_x_days.delay(patient_data, days)
         resting_hr = 0
         HRdata = {}
         HRaverage = {}
